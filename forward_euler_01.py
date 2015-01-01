62ed36025cbb90104becf3095e11d9dd5a6be334
Mohamed Talaat Harb <mohamed.talaatharb@eng.asu.edu.eg>
2015-01-01 19:38:49 +0200

    Creating the forward_euler_01 file

---
+++ forward_euler_01.py
@@ -0,0 +1,29 @@
+import numpy
+import matplotlib.pyplot
+
+def forward_euler():
+    h = 0.1
+    g = 9.81
+    
+    num_steps = 50
+    t = numpy.zeros(num_steps + 1)
+    x = numpy.zeros(num_steps + 1)
+    v = numpy.zeros(num_steps + 1)
+    
+    for step in range(num_steps):
+        t[step + 1] = #
+        x[step + 1] = #
+        v[step + 1] = #
+    
+    return t, x, v
+    
+t, x, v = forward_euler()
+
+axes_height = matplotlib.pyplot.subplot(211)
+matplotlib.pyplot.plot(t, x)
+axes_velocity = matplotlib.pyplot.subplot(212)
+matplotlib.pyplot.plot(t, v)
+axes_height.set_ylabel('Height in m')
+axes_velocity.set_ylabel('Velocity in m/s')
+axes_velocity.set_xlabel('Time in s')
+matplotlib.pyplot.show()
